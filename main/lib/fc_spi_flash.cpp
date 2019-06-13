4ba78a83174aa988c22f56257e3515ed9c7e60fb
Jackson Ming Hu <huming2207@gmail.com>
2019-06-13 23:45:44 +1000

    spi_flash: implement chip erase/status reg/probe chip

--- main/lib/fc_spi_flash.cpp
+++ main/lib/fc_spi_flash.cpp
@@ -1,81 +1,126 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include "fc_spi_flash.hpp"
 
 using namespace fc;
 
-esp_err_t spi_flash::sector_erase(uint32_t addr)
+spi_flash::spi_flash() : hal(fc_hal::get_hal())
 {
-    return 0;
+    probe_flash();
 }
 
-esp_err_t spi_flash::chip_erase()
+esp_err_t spi_flash::sector_erase(uint32_t start_addr, size_t len, uint32_t timeout_ms)
 {
-    return 0;
+    return ESP_OK;
 }
 
-esp_err_t spi_flash::block_erase_32(uint32_t addr)
+esp_err_t spi_flash::chip_erase(uint32_t timeout_ms)
 {
-    return 0;
+    esp_err_t ret = hal.spi_write(CHIP_ERASE);
+    ret = ret ?: wait_when_busy(timeout_ms);
+    return ret;
 }
 
-esp_err_t spi_flash::block_erase_64(uint32_t addr)
+esp_err_t spi_flash::block_erase_32(uint32_t start_addr, size_t len, uint32_t timeout_ms)
 {
-    return 0;
+    return ESP_OK;
 }
 
-esp_err_t spi_flash::page_write(uint32_t addr, uint32_t *payload)
+esp_err_t spi_flash::block_erase_64(uint32_t start_addr, size_t len, uint32_t timeout_ms)
 {
-    return 0;
+    return ESP_OK;
 }
 
-esp_err_t spi_flash::byte_read(uint32_t addr, uint32_t *payload, size_t length)
+esp_err_t spi_flash::page_write(uint32_t addr, const uint8_t *payload, size_t len)
 {
-    return 0;
+    return ESP_OK;
+}
+
+esp_err_t spi_flash::byte_read(uint32_t addr, const uint32_t *payload, size_t len)
+{
+    return ESP_OK;
 }
 
 bool spi_flash::get_busy()
 {
-    return false;
+    uint8_t result = 0;
+    esp_err_t ret = hal.spi_read(READ_STATUS_REG_1, &result, 1);
+    if(ret != ESP_OK) return true;
+
+    return (result & 0x01u) == 1; // Probe Bit 0
+}
+
+esp_err_t spi_flash::wait_when_busy(uint32_t timeout_ms)
+{
+    bool is_busy = true;
+    while(is_busy && timeout_ms > 1) {
+        is_busy = get_busy();
+        timeout_ms--;
+        vTaskDelay(pdMS_TO_TICKS(3));
+    }
+
+    if(timeout_ms < 1) return ESP_ERR_TIMEOUT;
+    else return ESP_OK;
 }
 
 uint32_t spi_flash::get_total_size()
 {
-    return 0;
+    return ESP_OK;
 }
 
 uint16_t spi_flash::get_page_size()
 {
-    return 0;
+    return ESP_OK;
 }
 
-uint8_t spi_flash::get_mf_id()
+esp_err_t spi_flash::get_jedec_id(spi_flash_ids& ids)
 {
-    return 0;
-}
+    uint8_t result[3] = { 0 };
+    esp_err_t ret = hal.spi_read(READ_JEDEC_ID, result, 3);
+    if(ret != ESP_OK) return ret;
 
-uint8_t spi_flash::get_dev_id()
-{
-    return 0;
+    ids.mf_id = result[0];
+    ids.chip_id = (uint16_t)((uint8_t)(result[1] << 8u) | result[2]); // Cast it to shut up Clang-tidy warning
+
+    return ESP_OK;
 }
 
 uint64_t spi_flash::get_unique_id()
 {
-    return 0;
+    return ESP_OK;
 }
 
 esp_err_t spi_flash::chip_wp_unlock()
 {
-    return 0;
+    return ESP_OK;
 }
 
 esp_err_t spi_flash::chip_pwr_off()
 {
-    return 0;
+    return ESP_OK;
 }
 
 esp_err_t spi_flash::chip_wp_relock()
 {
-    return 0;
+    return ESP_OK;
 }
 
+esp_err_t spi_flash::probe_flash()
+{
+    spi_flash_ids ids{};
+    esp_err_t ret = get_jedec_id(ids);
+    if(ret != ESP_OK) return ret;
 
+    chip_info = fc_spi_chips[ids];
 
+    return ESP_OK;
+}
+
+esp_err_t spi_flash::override_flash(spi_flash_ids &ids)
+{
+    chip_info = fc_spi_chips[ids];
+    if(chip_info.full_size_kb == 0) return ESP_ERR_NOT_FOUND; // If not found, operator[] will make a dummy object
+
+    return ESP_OK;
+}
--- main/lib/include/fc_spi_flash.hpp
+++ main/lib/include/fc_spi_flash.hpp
@@ -3,29 +3,47 @@
 #include <cstdint>
 
 #include <esp_err.h>
+#include "fc_hal.hpp"
+#include "fc_spi_chips.hpp"
 
 namespace fc
 {
+    typedef enum {
+        READ_STATUS_REG_1   = 0x05U,
+        READ_UNIQUE_ID      = 0x90U,
+        READ_JEDEC_ID       = 0x92U,
+        CHIP_ERASE          = 0xC7U,
+    } spi_flash_cmd;
+
     class spi_flash
     {
         public:
-            esp_err_t sector_erase(uint32_t addr);
-            esp_err_t block_erase_32(uint32_t addr);
-            esp_err_t block_erase_64(uint32_t addr);
-            esp_err_t chip_erase();
+            spi_flash();
+            esp_err_t sector_erase(uint32_t start_addr, size_t len, uint32_t timeout_ms = 500);
+            esp_err_t block_erase_32(uint32_t start_addr, size_t len, uint32_t timeout_ms = 5000);
+            esp_err_t block_erase_64(uint32_t start_addr, size_t len, uint32_t timeout_ms = 5000);
+            esp_err_t chip_erase(uint32_t timeout_ms = 15000);
 
-            esp_err_t page_write(uint32_t addr, uint32_t payload[3]);
-            esp_err_t byte_read(uint32_t addr, uint32_t *payload, size_t length);
+            esp_err_t page_write(uint32_t addr, const uint8_t *payload, size_t len);
+            esp_err_t byte_read(uint32_t addr, const uint32_t *payload, size_t len);
 
             bool get_busy();
+            esp_err_t wait_when_busy(uint32_t timeout_ms = 10000);
             uint32_t get_total_size();
             uint16_t get_page_size();
-            uint8_t get_mf_id();
-            uint8_t get_dev_id();
+            esp_err_t get_jedec_id(spi_flash_ids& ids);
             uint64_t get_unique_id();
 
             esp_err_t chip_wp_unlock();
             esp_err_t chip_pwr_off();
             esp_err_t chip_wp_relock();
+
+            esp_err_t probe_flash();
+            esp_err_t override_flash(spi_flash_ids &ids);
+
+        private:
+            fc_hal& hal;
+            bool is_4ba = false;
+            spi_flash_info chip_info{};
     };
 };
