42800fb6b6966500e384c03e40a435ca0e39efcf
Jackson Ming Hu <huming2207@gmail.com>
2019-06-27 23:15:13 +1000

    wifi_manager: decouple config with start/stop functions

--- main/sys/include/wifi_manager.hpp
+++ main/sys/include/wifi_manager.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <string>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+
 #include <nvs.h>
 #include <nvs_flash.h>
 #include <esp_log.h>
@@ -8,14 +12,44 @@
 
 namespace fc::sys
 {
+    namespace sysdef
+    {
+        enum wifi_event_bit
+        {
+            WIFI_STA_CONNECTED                  = BIT0,
+            WIFI_STA_CONNECTED_IPV6             = BIT1,
+            WIFI_AP_DEVICE_CONNECT              = BIT2,
+            WIFI_AP_DEVICE_DISCONNECT           = BIT3,
+            WIFI_STA_CONNECT_RETRY_MAXIMUM      = BIT4
+        };
+    }
+
     class wifi_manager
     {
         private:
-            static void wifi_nvs_preinit();
+            wifi_manager();
+            static uint8_t sta_retry_cnt;
+            static uint8_t sta_max_retry;
+            static EventGroupHandle_t wifi_event_group;
+            static ip6_addr_t ip6_addr;
+            static ip_addr_t ip_addr;
 
         public:
-            static esp_err_t start_ap(const std::string &ssid, const std::string &passwd);
-            static esp_err_t start_sta(const std::string &ssid, const std::string &passwd);
+            static wifi_manager& get_instance()
+            {
+                static wifi_manager instance;
+                return instance;
+            };
+            wifi_manager(wifi_manager const &) = delete;
+            void operator=(wifi_manager const &) = delete;
+
+            void set_sta_max_retry(uint8_t _sta_max_retry);
+            esp_err_t start_ap();
+            esp_err_t start_sta();
+            esp_err_t set_ap_config(const std::string &ssid, const std::string &passwd, uint8_t channel = 3);
+            esp_err_t set_sta_config(const std::string& ssid, const std::string& passwd);
+            esp_err_t clear_wifi_config();
+
     };
 
 }
--- main/sys/wifi_manager.cpp
+++ main/sys/wifi_manager.cpp
@@ -12,59 +12,130 @@
 
 using namespace fc::sys;
 
-esp_err_t wifi_manager::start_ap(const std::string &ssid, const std::string &passwd)
+wifi_manager::wifi_manager()
 {
+    // nvs_flash_init seems to be fine if run more than once in one instance
+    auto ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // WiFi event group init
+    wifi_manager::wifi_event_group = xEventGroupCreate();
+    if(wifi_manager::wifi_event_group == nullptr) {
+        ESP_LOGI(TAG, "Failed to initialise WiFi event group!");
+        return;
+    }
+
+    // STA max retry count
+    set_sta_max_retry(30);
+
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+    // Register WiFi event handler
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, []
-                    (void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-                       if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-                           auto* event = (wifi_event_ap_staconnected_t*) event_data;
-                           ESP_LOGI(TAG, "Station " MACSTR " joined, AID=%d",
-                                    MAC2STR(event->mac), event->aid);
-                       } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-                           auto* event = (wifi_event_ap_stadisconnected_t*) event_data;
-                           ESP_LOGI(TAG, "Station " MACSTR " left, AID=%d",
-                                    MAC2STR(event->mac), event->aid);
-                       }
-                    },
-                    nullptr));
+        (void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+            if (event_id == WIFI_EVENT_AP_STACONNECTED) {
 
+               auto* event = (wifi_event_ap_staconnected_t*) event_data;
+               ESP_LOGI(TAG, "Station " MACSTR " joined, AID=%d",
+                       MAC2STR(event->mac), event->aid);
+               xEventGroupSetBits(wifi_manager::wifi_event_group, sysdef::WIFI_AP_DEVICE_CONNECT);
+
+            } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+
+               auto* event = (wifi_event_ap_stadisconnected_t*) event_data;
+               ESP_LOGI(TAG, "Station " MACSTR " left, AID=%d",
+                       MAC2STR(event->mac), event->aid);
+               xEventGroupSetBits(wifi_manager::wifi_event_group, sysdef::WIFI_AP_DEVICE_DISCONNECT);
+
+            } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+
+               if (wifi_manager::sta_retry_cnt < wifi_manager::sta_max_retry) {
+                   esp_wifi_connect();
+                   xEventGroupClearBits(wifi_manager::wifi_event_group, sysdef::WIFI_STA_CONNECTED);
+                   wifi_manager::sta_max_retry++;
+                   ESP_LOGI(TAG, "Retry connecting to the AP");
+               }
+
+               ESP_LOGE(TAG,"Connect to the AP failed, maximum retry exceeded");
+               xEventGroupSetBits(wifi_manager::wifi_event_group, sysdef::WIFI_STA_CONNECT_RETRY_MAXIMUM);
+
+            } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+
+               auto* event = (ip_event_got_ip_t*) event_data;
+               ESP_LOGI(TAG, "Got IPv4 from AP: %s", ip4addr_ntoa(&event->ip_info.ip));
+               std::memcpy(&wifi_manager::ip_addr, &event->ip_info.ip, sizeof(ip_addr_t));
+               wifi_manager::sta_retry_cnt = 0;
+               xEventGroupSetBits(wifi_manager::wifi_event_group, sysdef::WIFI_STA_CONNECTED);
+
+            } else if (event_base == IP_EVENT && event_id == IP_EVENT_GOT_IP6) {
+
+               auto* event = (ip_event_got_ip6_t*) event_data;
+               ESP_LOGI(TAG, "Got IPv6 from AP: %s", ip6addr_ntoa(&event->ip6_info.ip));
+               std::memcpy(&wifi_manager::ip6_addr, &event->ip6_info.ip, sizeof(ip6_addr_t));
+               wifi_manager::sta_retry_cnt = 0;
+               xEventGroupSetBits(wifi_manager::wifi_event_group, sysdef::WIFI_STA_CONNECTED);
+
+            }
+            },
+            nullptr));
+}
+
+esp_err_t wifi_manager::start_ap()
+{
+    ESP_LOGI(TAG, "Starting AP...");
+
+    auto ret = esp_wifi_set_mode(WIFI_MODE_AP);
+    ret = ret ?: esp_wifi_start();
+
+    ESP_LOGI(TAG, "AP started!");
+
+    return ret;
+}
+
+esp_err_t wifi_manager::start_sta()
+{
+    ESP_LOGI(TAG, "Starting STA...");
+
+    auto ret = esp_wifi_set_mode(WIFI_MODE_STA);
+    ret = ret ?: esp_wifi_start();
+
+    ESP_LOGI(TAG, "AP started!");
+
+    return ret;
+}
+
+esp_err_t wifi_manager::set_ap_config(const std::string &ssid, const std::string &passwd, uint8_t channel = 3)
+{
     wifi_config_t wifi_config{};
     wifi_config.ap.authmode = passwd.empty() ? WIFI_AUTH_OPEN : WIFI_AUTH_WPA2_PSK;
     wifi_config.ap.beacon_interval = 100;
-    wifi_config.ap.channel = 3;
+    wifi_config.ap.channel = channel;
     std::strcpy(reinterpret_cast<char *>(wifi_config.sta.ssid), ssid.c_str());
     std::strcpy(reinterpret_cast<char *>(wifi_config.sta.password), passwd.c_str());
     wifi_config.ap.ssid_len = ssid.size();
 
-    ESP_LOGI(TAG, "Starting AP...");
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "AP started!");
-
-    return ESP_OK;
+    return esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config);
 }
 
-esp_err_t wifi_manager::start_sta(const std::string &ssid, const std::string &passwd)
+esp_err_t wifi_manager::set_sta_config(const std::string &ssid, const std::string &passwd)
 {
     return 0;
 }
 
-void wifi_manager::wifi_nvs_preinit()
+esp_err_t wifi_manager::clear_wifi_config()
 {
-    auto ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
+    return esp_wifi_restore();
+}
 
-    ESP_ERROR_CHECK(ret);
+void wifi_manager::set_sta_max_retry(uint8_t _sta_max_retry)
+{
+    wifi_manager::sta_max_retry = _sta_max_retry;
 }
