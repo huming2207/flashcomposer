6aa1926cb020ae523c8d2fbd80ee107c7ff201dc
Jackson Ming Hu <huming2207@gmail.com>
2019-06-29 10:26:40 +1000

    wifi_mgr: declare static members

--- main/fc_main.cpp
+++ main/fc_main.cpp
@@ -8,6 +8,8 @@
 #include "spi_flash.hpp"
 
 #include <ArduinoJson.h>
+#include <nvs_flash.h>
+#include <wifi_manager.hpp>
 
 #define TAG "fc_main"
 
@@ -25,21 +27,28 @@ void app_main()
 
 void fc_main::start_main()
 {
-    hardware::spi_flash flash;
-    esp_err_t ret = flash.chip_reset();
-    ret = ret ?: flash.probe_flash();
-    while(ret != ESP_OK && ret != ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGI(TAG, "Failed to probe the flash, waiting for another 1 sec to retry...");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        ret = flash.probe_flash();
-    }
+//    hardware::spi_flash flash;
+//    esp_err_t ret = flash.chip_reset();
+//    ret = ret ?: flash.probe_flash();
+//    while(ret != ESP_OK && ret != ESP_ERR_NOT_SUPPORTED) {
+//        ESP_LOGI(TAG, "Failed to probe the flash, waiting for another 1 sec to retry...");
+//        vTaskDelay(pdMS_TO_TICKS(1000));
+//        ret = flash.probe_flash();
+//    }
+//
+//    if(ret == ESP_ERR_NOT_SUPPORTED) {
+//        ESP_LOGE(TAG, "This SPI flash is not supported!");
+//    }
+//
+//    auto chip_info = flash.get_chip_info();
+//
+//    ESP_LOGI(TAG, "Detected %s %s, size %u KB",
+//            chip_info.mf_name.c_str(), chip_info.chip_name.c_str(), chip_info.full_size_kb);
 
-    if(ret == ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGE(TAG, "This SPI flash is not supported!");
-    }
-
-    auto chip_info = flash.get_chip_info();
-
-    ESP_LOGI(TAG, "Detected %s %s, size %u KB",
-            chip_info.mf_name.c_str(), chip_info.chip_name.c_str(), chip_info.full_size_kb);
+    sys::wifi_manager& wifi_man = sys::wifi_manager::get_instance();
+    auto ret = wifi_man.set_ap_config("esp_test", "whatever123");
+    ESP_LOGI(TAG, "Set config returns %u", ret);
+    ret = ret ?: wifi_man.start_ap();
+    ESP_LOGI(TAG, "Start AP returns %u", ret);
+    vTaskDelay(portMAX_DELAY);
 }
